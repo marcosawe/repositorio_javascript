6e3ae61df79f8e925bff8a05c545f6a93456ba85
Marcos Eduardo Fernandes Domingues <maresduardo420@gmail.com>
2022-09-25 16:34:38 -0300

    main

--- Validando_CPF/main.js
+++ Validando_CPF/main.js
@@ -1,45 +1,54 @@
-
-
-function ValidaCPF(cpf){
-    Object.defineProperty(this,'cpfLimpo',{
-        get:function(){
-            return cpf.replace(/\D+/g, "")
-        }
-    });
-}
-
-ValidaCPF.prototype.valida = function(){
-    if(typeof this.cpfLimpo === undefined) return false;
-    if(this.cpfLimpo.length !== 11) return false;
-    if(this.sequencia()) return false;
-
-    const cpfParcial = this.cpfLimpo.slice(0,-2);
-    const digito1 = this.criaDigito(cpfParcial);
-    const digito2 = this.criaDigito(cpfParcial + digito1);
-    const novoCpf = cpfParcial + digito1 + digito2
-    return novoCpf === this.cpfLimpo
-}
-
-ValidaCPF.prototype.criaDigito = function(cpfParcial){
-    const arrayParcial = Array.from(cpfParcial)
-
-    let regressivo = arrayParcial.length + 1;
-    const total = arrayParcial.reduce((ac,val)=>{
-        ac += (regressivo * Number(val));
-        regressivo--;
-        return ac
-    },0)
-
-    const digito = 11 - (total % 11)
-    return digito > 9 ? '0' : String(digito);
-
-}
-
-ValidaCPF.prototype.sequencia = function(){
-    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
-    return sequencia === this.cpfLimpo;
-}
-
-const cpf = new ValidaCPF(prompt('Digite seu CPF'));
-cpf.cpfLimpo
-console.log(cpf.valida());
+// 705.484.450-52 070.987.720-03
+class ValidaCPF {
+    constructor(cpfEnviado) {
+      Object.defineProperty(this, 'cpfLimpo', {
+        writable: false,
+        enumerable: true,
+        configurable: false,
+        value: cpfEnviado.replace(/\D+/g, '')
+      });
+    }
+  
+    éSequência() {
+      return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+  
+    geraNovoCpf() {
+      const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+      const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+      const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+      this.novoCPF = cpfSemDigitos + digito1 + digito2;
+    }
+  
+    static geraDigito(cpfSemDigitos) {
+      let total = 0;
+      let reverso = cpfSemDigitos.length + 1;
+  
+      for(let stringNumerica of cpfSemDigitos) {
+        total += reverso * Number(stringNumerica);
+        reverso--;
+      }
+  
+      const digito = 11 - (total % 11);
+      return digito <= 9 ? String(digito) : '0';
+    }
+  
+    valida() {
+      if(!this.cpfLimpo) return false;
+      if(typeof this.cpfLimpo !== 'string') return false;
+      if(this.cpfLimpo.length !== 11) return false;
+      if(this.éSequência()) return false;
+      this.geraNovoCpf();
+  
+      return this.novoCPF === this.cpfLimpo;
+    }
+  }
+  
+  let validacpf = new ValidaCPF('070.987.720-03');
+  // validacpf = new ValidaCPF('999.999.999-99');
+  
+  if (validacpf.valida()) {
+    console.log('CPF válido');
+  } else {
+    console.log('CPF inválido');
+  }
---
+++ validando_formulario/modelo_html_css/assets/js/main.js
@@ -0,0 +1,13 @@
+class ValidaFormulario{
+    constructor(){
+        this.formulario = document.querySelector('#formulario')
+        this.eventos()
+    }
+    eventos(){
+        this.formulario.addEventListener("submit", e=>{
+            
+        })
+    }
+}
+
+const valida = new ValidaFormulario()
